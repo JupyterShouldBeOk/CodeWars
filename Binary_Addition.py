46b31f1b375b2391534f12aeae07657ae4dbf6d0
Guilherme Castro <guilherme.castro.f.araujo@gmail.com>
2021-06-11 22:29:05 -0300

    Solved

---
+++ Binary_Addition.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 20:37:39 2021
+
+@author: Guilherme
+"""
+def add_binary(a,b):
+    sum = a + b
+    sumbin = format(sum, "b")
+    return sumbin
+
